1be527eed1bd5e8a7638f208a38d128b05d793aa
Anton Sokolovskyi <sokolovskii.anton@gmail.com>
2024-01-18 02:37:16 +0200

    add delete file endpoint

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,5 +1,7 @@
 import {
+  Body,
   Controller,
+  Delete,
   Post,
   UploadedFile,
   UseGuards,
@@ -9,6 +11,9 @@ import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { FileService } from 'src/file/file.service';
 import { FileResponseInterface } from 'src/file/types/fileResponse.interface';
+import { DeleteFileDto } from 'src/file/dto/deleteFile.dto';
+import { PromiseResult } from 'aws-sdk/lib/request';
+import { AWSError, S3 } from 'aws-sdk';
 
 @Controller('file')
 export class FileController {
@@ -18,5 +23,12 @@ export class FileController {
   @UseInterceptors(FileInterceptor('file'))
   async uploadFile(@UploadedFile() file: any): Promise<FileResponseInterface> {
     return await this.fileService.save(file);
+  }
+  @Delete()
+  @UseGuards(AuthGuard)
+  async deleteFile(
+    @Body() deleteFileDto: DeleteFileDto,
+  ): Promise<PromiseResult<S3.DeleteObjectOutput, AWSError>> {
+    return await this.fileService.deleteImage(deleteFileDto);
   }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,18 +1,19 @@
 import { BadRequestException, Injectable, UploadedFile } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { S3 } from 'aws-sdk';
+import { AWSError, S3 } from 'aws-sdk';
 import { FileResponseInterface } from 'src/file/types/fileResponse.interface';
+import { DeleteFileDto } from 'src/file/dto/deleteFile.dto';
+import { PromiseResult } from 'aws-sdk/lib/request';
 @Injectable()
 export class FileService {
+  s3 = new S3({
+    credentials: {
+      accessKeyId: this.configService.get<string>('AWS_ACCESS_ID'),
+      secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY'),
+    },
+  });
   constructor(private configService: ConfigService) {}
   async save(@UploadedFile() file): Promise<FileResponseInterface> {
-    const s3 = new S3({
-      credentials: {
-        accessKeyId: this.configService.get<string>('AWS_ACCESS_ID'),
-        secretAccessKey: this.configService.get<string>('AWS_SECRET_KEY'),
-      },
-    });
-
     const fileContent = await file.buffer;
 
     const params = {
@@ -24,8 +25,21 @@ export class FileService {
     };
     // Upload file to S3
     try {
-      const uploadedFile = await s3.upload(params).promise();
+      const uploadedFile = await this.s3.upload(params).promise();
       return { url: uploadedFile.Location, key: uploadedFile.Key };
+    } catch (error) {
+      throw new BadRequestException('Bad request');
+    }
+  }
+  async deleteImage(
+    deleteFileDto: DeleteFileDto,
+  ): Promise<PromiseResult<S3.DeleteObjectOutput, AWSError>> {
+    const params = {
+      Bucket: this.configService.get<string>('AWS_BUCKET'),
+      Key: deleteFileDto.key,
+    };
+    try {
+      return await this.s3.deleteObject(params).promise();
     } catch (error) {
       throw new BadRequestException('Bad request');
     }
